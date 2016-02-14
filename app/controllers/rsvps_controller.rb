d4e7464136cf440355bc8ff55f331c0570ef3dc7
Larry Reaves <larry@yrral.net>
2016-02-14 15:47:27 -0500

    form updates

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -24,7 +24,7 @@ class RsvpsController < ApplicationController
 
     respond_to do |format|
       if @rsvp.save
-        format.html { redirect_to @rsvp, notice: 'Rsvp was successfully created.' }
+        format.html { redirect_to @rsvp }
         format.json { render :show, status: :created, location: @rsvp }
       else
         format.html { render :new }
